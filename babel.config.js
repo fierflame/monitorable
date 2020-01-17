534d019947c894c4b0281dd160a90e93e21ca3ba
Fierflame <git@fierflame.com>
2020-01-17 12:27:26 +0800

    chore: 搭建开发环境

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	plugins: [
+		'@babel/plugin-syntax-async-generators',
+		'@babel/plugin-proposal-class-properties',
+		'@babel/plugin-proposal-do-expressions',
+		'@babel/plugin-proposal-function-bind',
+		'@babel/plugin-proposal-logical-assignment-operators',
+		'@babel/plugin-proposal-numeric-separator',
+		'@babel/plugin-proposal-object-rest-spread',
+		'@babel/plugin-proposal-optional-catch-binding',
+		'@babel/plugin-proposal-optional-chaining',
+		'@babel/plugin-proposal-private-methods',
+		'@babel/plugin-proposal-throw-expressions',
+		'@babel/plugin-transform-typescript',
+	],
+};
---
+++ rollup.config.js
@@ -0,0 +1,90 @@
+
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import dts from 'rollup-plugin-dts';
+import replace from 'rollup-plugin-replace';
+import { terser } from 'rollup-plugin-terser';
+import fsFn from 'fs';
+const { name, version, author, license } = JSON.parse(fsFn.readFileSync('./package.json'));
+
+const beginYear = 2020;
+const year = new Date().getFullYear();
+
+const banner = `
+/*!
+ * ${ name } v${ version }
+ * (c) ${ beginYear === year ? beginYear : `${ beginYear }-${ year }` } ${ author }
+ * @license ${ license }
+ */
+`;
+const GlobalName = name.replace(/(?:^|-)([a-z])/g, (_, s) => s.toUpperCase());
+export default [
+	{
+		input: 'src/index.ts',
+		output: [
+			{
+				file: `dist/${ name }.esm.js`,
+				sourcemap: true,
+				format: 'esm',
+				banner,
+			},
+			{
+				file: `dist/${ name }.common.js`,
+				sourcemap: true,
+				format: 'cjs',
+				banner,
+				// exports: 'default',
+			},
+			{
+				file: `dist/${ name }.js`,
+				sourcemap: true,
+				format: 'umd',
+				name: GlobalName,
+				banner,
+				// exports: 'default',
+			},
+		],
+		plugins: [
+			resolve({ extensions: [ '.mjs', '.js', '.jsx', '.json', '.ts', '.tsx' ] }),
+			babel({ extensions: [ '.mjs', '.js', '.jsx', '.es', '.ts', '.tsx' ] }),
+			replace({ __VERSION__: version }),
+		],
+	},
+	{
+		input: 'src/index.ts',
+		output: [
+			{
+				file: `dist/${ name }.esm.min.js`,
+				sourcemap: true,
+				format: 'esm',
+				banner,
+			},
+			{
+				file: `dist/${ name }.common.min.js`,
+				sourcemap: true,
+				format: 'cjs',
+				banner,
+				// exports: 'default',
+			},
+			{
+				file: `dist/${ name }.min.js`,
+				sourcemap: true,
+				format: 'umd',
+				name: GlobalName,
+				banner,
+				// exports: 'default',
+			},
+		],
+		plugins: [
+			resolve({ extensions: [ '.mjs', '.js', '.jsx', '.json', '.ts', '.tsx' ] }),
+			babel({ extensions: [ '.mjs', '.js', '.jsx', '.es', '.ts', '.tsx' ] }),
+			replace({ __VERSION__: version }),
+			terser(),
+		],
+	},
+	{
+		input: [ 'src/index.ts' ],
+		output: [ { file: 'types.d.ts', format: 'esm', banner } ],
+		plugins: [ dts() ],
+	},
+];
