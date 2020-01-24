b3caf913b201ba33cd25e403e428293558a69ab3
Fierflame <git@fierflame.com>
2020-01-24 21:43:26 +0800

    feat: 增加 printError 方法已支持自定义错误输出

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,30 @@
+
+let printErrorLog: undefined | ((info: any) => void);
+/** 设置或移除错误打印函数 */
+export function printError(fn?: (info: any) => void): void;
+/** 打印错误 */
+export function printError(info: any, print: true): void;
+/** 打印错误 */
+export function printError(info: any): void;
+export function printError(info?: string | Error | ((info: any) => void), print = false) {
+	if (!print && (typeof info === 'function' || info === undefined)) {
+		printErrorLog = info;
+		return;
+	}
+	if (typeof printErrorLog === 'function') {
+		printErrorLog(info);
+		return;
+	}
+	console.error(info);
+}
+
 /** 回调函数安全化处理 */
 export function safeify<T extends any[]>(fn: (...p: T) => void): (...p: T) => void {
 	return (...p) => {
 		try {
 			fn(...p);
 		} catch(e) {
-			console.error(e);
+			printError(e, true);
 		}
 	};
 }
